79939a602149f9eeea0632b401d82773166caf28
Sellyon <49185185+Sellyon@users.noreply.github.com>
2019-10-28 14:14:04 +0100

    add loading spinner

--- src/components/news.js
+++ src/components/news.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { Paper } from '@material-ui/core/';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class News extends React.Component {
   render() {
@@ -18,14 +17,18 @@ class News extends React.Component {
         <h3>{newsList && newsList.length > 0 && newsList[newsSelected].title}</h3>
         <h4>par {newsList && newsList.length > 0 && newsList[newsSelected].author}</h4>
         <p style={{ display: "none" }}>{newsList && newsList.length > 0 && newsList[newsSelected].content}</p>
-        <p>publié le : {newsList && newsList.length > 0 && newsList[newsSelected].date}</p>
+        <p>publié depuis : {Math.round((newsList && newsList.length > 0 && Date.now()-newsList[newsSelected].date)/3600000)}h</p>
         <p>likes : {newsList && newsList.length > 0 && newsList[newsSelected].likes.length}</p>
       </div>
     );
 
     const errorMessage = <span>{this.props.loadingErrorMessage}</span>;
 
-    const loadingMessage = <span>Loading...</span>;
+    const loadingMessage = (
+      <div>
+        <CircularProgress color="secondary" />
+      </div>
+    );
 
     console.log(isLoading)
 
